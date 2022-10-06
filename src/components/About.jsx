f28bf59a9f490f8bc3b924eb180afb40c4589cce
Kim Chung <kchung68@my.bcit.ca>
2022-10-06 14:55:42 -0700

    adjusted about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,13 @@ const About = () => {
                     <p className='text-4xl font-bold text-cyan-700'>What do I bring?</p>
                 </div>
                 <p className='pb-5 text-base text-black font-extralight'>
-                    I'm going to finish my first year of BCIT Computer Systems program this December and I'm looking for a Co-op opportunity starting from January 2023. Wait, one-year experience is a bit short, isn't it? Well, here is my offer to make it up to you:
+                    I'm going to finish my first year of BCIT Computer Systems program this December and I'm looking for a Co-op opportunity starting from January 2023.
+                </p>
+                <p className='pb-5 text-base text-black font-extralight'>
+                    Wait, one-year experience is a bit short, isn't it?
+                </p>
+                <p className='pb-5 text-base text-black font-extralight'>
+                    But from a different perspective, doesn't it indicate my learning potential when I was able to learn, utilize my knowledge, and build actual products in such a short period of time? Also, here is what I bring to table:
                 </p>
                 <p className='text-xl text-black font-extralight'>
                     <ul className="font-normal text-cyan-700">
@@ -27,7 +33,7 @@ const About = () => {
                     </ul>
                 </p>
                 <p className='text-base text-black font-extralight pt-5'>
-                Besides, I have a diverse work experience ranging from Accounting to Customer Service. This means I come with interpersonal skills and attention to details! If my offer sounds fair to you, let's get in touch! What's more? Please check out my projects and technical experience right below.
+                    Besides, I have a diverse work experience ranging from Accounting to Customer Service. This means I come with interpersonal skills and attention to details! If my offer sounds fair to you, let's get in touch! What's more? Please check out my projects and technical experience right below.
                 </p>
 
 
