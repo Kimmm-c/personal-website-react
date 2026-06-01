f0eb3c22502c08d7e81e5b8fe12137d25846bb06
kapybara <97310663+Kimmm-c@users.noreply.github.com>
2026-06-01 15:25:17 -0700

    commented out non-gamedev projects

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -59,86 +59,86 @@ const Projects = () => {
             ],
             title: "Allez"
         },
-        {
-            src: escaper,
-            href: 'https://github.com/Kimmm-c/escaper-game-dev',
-            video: '',
-            contents: [
-                {
-                    title: "Project Description",
-                    text: "This project is focused on learning C++. Escaper is a C++ text-based adventure game where players navigate a grid-based map using the arrow keys, starting from the top-left corner and aiming to reach the bottom-right corner while avoiding walls. The difficulty level and map size are customizable, offering a new challenge with every playthrough."
-                },
-                {
-                    title: "Technologies",
-                    text: "C++"
-                }
-            ],
-            title: "Escaper"
-        },
-        {
-            src: '',
-            href: '',
-            video: appDemo,
-            contents: [
-                {
-                    title: "Project Description",
-                    text: "This project involves the development of a comprehensive e-commerce website, allowing users to browse products, add items to their carts, and proceed to checkout at their convenience. The payment system is integrated with Stripe, ensuring secure and reliable transactions. The website is built using modern web technologies, including Next.js for server-side rendering and React-based components, TailwindCSS for responsive and customizable styling, and React for dynamic front-end interactivity. Data management is handled by a PostgreSQL database, ensuring robust and scalable storage. The front-end of the application is deployed on Vercel, providing optimized performance and easy deployment workflows. The backend is hosted on an AWS EC2 instance, offering reliable server management and scalability. The database is securely hosted on AWS RDS, ensuring data integrity and accessibility."
-                },
-                {
-                    title: "Technologies",
-                    text: "JavaScript, Next.js, React, PostgreSQL, AWS EC2, AWS RDS, TailwindCSS."
-                }
-            ],
-            title: "E-commerce Website"
-        },
-        {
-            src: cli,
-            href: 'https://github.com/Kimmm-c/automate-create-remote-repo',
-            video: '',
-            contents: [
-                {
-                    title: "Project Description",
-                    text: "This tool simplifies setting up a local Git repo, creating a remote GitHub repo via the API, optionally adding a ‘.gitignore’ file, and handling staging, committing, and pushing—all in one seamless process. Built with object-oriented Python and available on PyPI, it offers a solution for efficient Git management."
-                },
-                {
-                    title: "Technologies",
-                    text: "Object-oriented Python."
-                }
-            ],
-            title: "CLI Automation Tool"
-        },
-        {
-            src: fitnesstracker,
-            href: 'https://github.com/Kimmm-c/fitness-tracker',
-            video: '',
-            contents: [
-                {
-                    title: "Project Description",
-                    text: "Embarking on an innovative journey into the realm of Machine Learning, this Python-based project harnesses the power of Mediapipe’s advanced Pose Estimation model to revolutionize workout monitoring. Designed as an experimental exploration, this endeavor pushes the boundaries of how pre-trained models can be practically applied to real-world scenarios."
-                },
-                {
-                    title: "Technologies",
-                    text: "Python, OpenCV, MediaPipe."
-                }
-            ],
-            title: "Fitness Tracker"
-        },
-        {
-            src: neo,
-            href: 'https://kimmmc.pythonanywhere.com/about/',
-            video: '',
-            contents: [
-                {
-                    title: "Project Description",
-                    text: "In the vast expanse of outer space, numerous objects orbit around us, some much closer than we might realize. Despite their seeming distance, objects as close as 70,000 kilometers have the potential to disrupt natural phenomena on Earth. It's crucial for us to understand the nature of these objects, particularly those that pose potential harm. \nOur SpaceGuard AI project is trained on NASA’s extensive Near-Earth Object dataset, which comprises over 90,000 entry points. This vast repository of data provides a comprehensive foundation for our model, enabling it to identify patterns and correlations that signal potential threats from space."
-                },
-                {
-                    title: "Technologies",
-                    text: "Python, Django, ML libraries such as Pandas, Scikit-learn, Matplotlib, etc.."
-                }
-            ],
-            title: 'SpaceGuard AI'
-        }
+        // {
+        //     src: escaper,
+        //     href: 'https://github.com/Kimmm-c/escaper-game-dev',
+        //     video: '',
+        //     contents: [
+        //         {
+        //             title: "Project Description",
+        //             text: "This project is focused on learning C++. Escaper is a C++ text-based adventure game where players navigate a grid-based map using the arrow keys, starting from the top-left corner and aiming to reach the bottom-right corner while avoiding walls. The difficulty level and map size are customizable, offering a new challenge with every playthrough."
+        //         },
+        //         {
+        //             title: "Technologies",
+        //             text: "C++"
+        //         }
+        //     ],
+        //     title: "Escaper"
+        // },
+        // {
+        //     src: '',
+        //     href: '',
+        //     video: appDemo,
+        //     contents: [
+        //         {
+        //             title: "Project Description",
+        //             text: "This project involves the development of a comprehensive e-commerce website, allowing users to browse products, add items to their carts, and proceed to checkout at their convenience. The payment system is integrated with Stripe, ensuring secure and reliable transactions. The website is built using modern web technologies, including Next.js for server-side rendering and React-based components, TailwindCSS for responsive and customizable styling, and React for dynamic front-end interactivity. Data management is handled by a PostgreSQL database, ensuring robust and scalable storage. The front-end of the application is deployed on Vercel, providing optimized performance and easy deployment workflows. The backend is hosted on an AWS EC2 instance, offering reliable server management and scalability. The database is securely hosted on AWS RDS, ensuring data integrity and accessibility."
+        //         },
+        //         {
+        //             title: "Technologies",
+        //             text: "JavaScript, Next.js, React, PostgreSQL, AWS EC2, AWS RDS, TailwindCSS."
+        //         }
+        //     ],
+        //     title: "E-commerce Website"
+        // },
+        // {
+        //     src: cli,
+        //     href: 'https://github.com/Kimmm-c/automate-create-remote-repo',
+        //     video: '',
+        //     contents: [
+        //         {
+        //             title: "Project Description",
+        //             text: "This tool simplifies setting up a local Git repo, creating a remote GitHub repo via the API, optionally adding a ‘.gitignore’ file, and handling staging, committing, and pushing—all in one seamless process. Built with object-oriented Python and available on PyPI, it offers a solution for efficient Git management."
+        //         },
+        //         {
+        //             title: "Technologies",
+        //             text: "Object-oriented Python."
+        //         }
+        //     ],
+        //     title: "CLI Automation Tool"
+        // },
+        // {
+        //     src: fitnesstracker,
+        //     href: 'https://github.com/Kimmm-c/fitness-tracker',
+        //     video: '',
+        //     contents: [
+        //         {
+        //             title: "Project Description",
+        //             text: "Embarking on an innovative journey into the realm of Machine Learning, this Python-based project harnesses the power of Mediapipe’s advanced Pose Estimation model to revolutionize workout monitoring. Designed as an experimental exploration, this endeavor pushes the boundaries of how pre-trained models can be practically applied to real-world scenarios."
+        //         },
+        //         {
+        //             title: "Technologies",
+        //             text: "Python, OpenCV, MediaPipe."
+        //         }
+        //     ],
+        //     title: "Fitness Tracker"
+        // },
+        // {
+        //     src: neo,
+        //     href: 'https://kimmmc.pythonanywhere.com/about/',
+        //     video: '',
+        //     contents: [
+        //         {
+        //             title: "Project Description",
+        //             text: "In the vast expanse of outer space, numerous objects orbit around us, some much closer than we might realize. Despite their seeming distance, objects as close as 70,000 kilometers have the potential to disrupt natural phenomena on Earth. It's crucial for us to understand the nature of these objects, particularly those that pose potential harm. \nOur SpaceGuard AI project is trained on NASA’s extensive Near-Earth Object dataset, which comprises over 90,000 entry points. This vast repository of data provides a comprehensive foundation for our model, enabling it to identify patterns and correlations that signal potential threats from space."
+        //         },
+        //         {
+        //             title: "Technologies",
+        //             text: "Python, Django, ML libraries such as Pandas, Scikit-learn, Matplotlib, etc.."
+        //         }
+        //     ],
+        //     title: 'SpaceGuard AI'
+        // }
     ]
 
     return (
